ec5064c6c9a77c50f560d9cd9052ff4830f4ac0b
Mike Merin <mikemerinweather@gmail.com>
2017-04-28 13:29:33 -0400

    Landmark test not working

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,38 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'landmarks/index'
+  end
+
+  get '/landmarks/new' do
+    @figures = Figure.all
+    @landmarks = Landmark.all
+    @titles = Figure.all.map { |figure| figure.titles }
+    erb :'landmarks/new'
+  end
+
+  get '/landmarks/:id' do
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    binding.pry
+    @landmark = Landmark.find(params[:id])
+    erb :'landmarks/edit'
+  end
+
+  patch '/landmarks/:id' do
+
+    @landmark = Landmark.find(params[:id])
+    @landmark.update(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
+  post '/landmarks' do
+    @landmark = Landmark.create(params[:landmark])
+    redirect "/landmarks/#{@landmark.id}"
+  end
+
 end
